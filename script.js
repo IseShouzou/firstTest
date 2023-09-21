964e5ef79e834dbe9bd436b1f2dab8faf1ed9c99
IseShouzou <144583628+IseShouzou@users.noreply.github.com>
2023-09-21 22:13:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ window.onload = function(){
     canvas.setAttribute("width", window.innerWidth )
     canvas.setAttribute("height", window.innerHeight )
 
+    let fovy = 45.0;
+
+    let pnts = [ [ 0.5, -0.5,  0.1 ],
+                 [ 0.5,  0.5,  0.1 ],
+                 [ 0.5,  0.5,  0.5 ],
+                 [ 0.5, -0.5,  0.5 ] ];
+
+
     window.addEventListener("deviceorientation", function(e){
         alpRad = ( e.alpha || 0) * Math.PI / 180.0;
         betRad = ( e.beta  || 0) * Math.PI / 180.0;
@@ -17,7 +25,7 @@ window.onload = function(){
 
     });
 
-    function calcEuler(){
+    function calcDCM(){
 
         let ca = Math.cos( alpRad );
         let sa = Math.sin( alpRad );
@@ -38,11 +46,17 @@ window.onload = function(){
         let m23 =   sg * sa - cg * sb * ca ;
         let m33 =   sg * ca + cg * sb * sa ;
 
-        let phi = 180.0 / Math.PI * Math.atan2(   m23, m33 );
-        let the = 180.0 / Math.PI * Math.atan2( - m13, Math.sqrt( m23 * m23 + m33 * m33) );
-        let psi = 180.0 / Math.PI * Math.atan2(   m12, m11 );
+        //let phi = 180.0 / Math.PI * Math.atan2(   m23, m33 );
+        //let the = 180.0 / Math.PI * Math.atan2( - m13, Math.sqrt( m23 * m23 + m33 * m33) );
+        //let psi = 180.0 / Math.PI * Math.atan2(   m12, m11 );
+        //return [ phi, the, psi ];
 
-        return [ phi, the, psi ];
+        //return [ [ m11, m12, m13 ],
+        //         [ m21, m22, m23 ],
+        //         [ m31, m32, m33 ] ];
+        return [ [ 1.0, 0.0, 0.0 ],
+                 [ 0.0, 1.0, 0.0 ],
+                 [ 0.0, 0.0, 1.0 ] ];
 
     }
 
@@ -61,24 +75,43 @@ window.onload = function(){
                         let W = canvas.width;
                         let H = canvas.height;
 
+                        let e = W / 2.0 / Math.tan( fovy / 2.0 * Math.PI / 180.0 );
+
+                        //console.log( W, H, e );
+
                         ctx.save();
 
                         ctx.fillStyle = "#ddd";
                         ctx.fillRect(0, 0, W, H );
 
-                        let euler = calcEuler();
-                        let Y = euler[1];
-
-                        ctx.font = "24px system";
-                        ctx.fillStyle = "#F55";
-                        ctx.textBaseline = "top";
-                        ctx.fillText("phi: " + Y , 20,  20);
+                        //ctx.font = "24px system";
+                        //ctx.fillStyle = "#F55";
+                        //ctx.textBaseline = "top";
+                        //ctx.fillText("X: " + X , 20,  20);
+                        //ctx.fillText("Y: " + Y , 20,  60);
+                        //console.log( pnts.length );
 
                         ctx.beginPath();
-                        ctx.moveTo( 100, Y + 100 );
-                        ctx.lineTo( 300, Y + 100 );
-                        ctx.lineTo( 300, Y + 400 );
-                        ctx.lineTo( 100, Y + 400 );
+
+                        let dcm = calcDCM();
+                        for( let k = 0; k < pnts.length; k++ ){
+                            let x = 0.0;
+                            let y = 0.0;
+                            let z = 0.0;
+                            for( let j = 0; j <3; j++ ){
+                                x += dcm[0][j] * pnts[k][j];
+                                y += dcm[1][j] * pnts[k][j];
+                                z += dcm[2][j] * pnts[k][j];
+                            }
+                            XX = W / 2.0 + e * z / x;
+                            YY = H / 2.0 - e * y / x;
+
+                            //console.log( XX, YY );
+
+                            if( k==0 ){ ctx.moveTo( XX, YY ); }
+                            else      { ctx.lineTo( XX, YY ); }
+                        }
+
                         ctx.closePath();
                         ctx.stroke();
                         ctx.clip();
@@ -105,3 +138,5 @@ window.onload = function(){
         .catch(e => alert("error" + e.message));
 
 };
+
+
